a7bbd7f7c0414300cb258af40fa7cd8145b4e3f8
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-27 21:41:33 -0300

    Ajuste na função de conexão entre back e o front

--- front-end/src/Services/Axios.js
+++ front-end/src/Services/Axios.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_BACK_END || '3001'}`,
+  baseURL: `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}`,
 });
 
-export const requestLogin = async (endpoint, body) => {
+export const requestAccess = async (endpoint, body) => {
   const { data } = await api.post(endpoint, body).catch((error) => error.response);
+  console.log({ data, api, endpoint });
   return data;
 };
 
-export const requestRegister = async (endpoint, body) => {
-  const { data } = await api.post(endpoint, body).catch((error) => error.response);
-  console.log(data);
-  return data;
-};
+// export const requestRegister = async (endpoint, body) => {
+//   const { data } = await api.post(endpoint, body).catch((error) => error.response);
+//   console.log(data);
+//   return data;
+// };
