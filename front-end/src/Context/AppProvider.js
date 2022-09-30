9e9633e04ce2ec3f2e139f25d901f6c4d7447dc0
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-30 16:54:52 -0300

    Adicionado ao Provider as funções de requisição de produtos e armazenamento das informações de acesso no Local Storage

--- front-end/src/Context/AppProvider.js
+++ front-end/src/Context/AppProvider.js
@@ -1,24 +1,43 @@
 import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import appContext from './AppContext';
+import { requestProducts } from '../Services/Axios';
 
 function AppProvider({ children }) {
   const [email, setEmail] = useState('');
-  const [btnLoginDisabled, setBtnLogin] = useState(true);
   const [name, setName] = useState('');
+  const [btnLoginDisabled, setBtnLogin] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const productsRequest = async () => {
+    const prodList = await requestProducts('/products');
+
+    setProducts(prodList);
+  };
 
   const memo = useMemo(() => {
+    const setLocalStorageAccessInfo = (user) => {
+      localStorage.setItem('name', JSON.stringify({ name: user.name }));
+      localStorage.setItem('email', JSON.stringify({ email }));
+      localStorage.setItem('role', JSON.stringify({ role: user.role }));
+      localStorage.setItem('token', JSON.stringify({ token: user.token }));
+    };
+
     const objApp = {
       email,
       btnLoginDisabled,
       name,
+      products,
       setEmail,
       setBtnLogin,
       setName,
+      setProducts,
+      productsRequest,
+      setLocalStorageAccessInfo,
     };
 
     return objApp;
-  }, [email, btnLoginDisabled, name]);
+  }, [email, btnLoginDisabled, name, products]);
 
   return (
     <appContext.Provider value={ memo }>
