82508830bf4e33a805f2ce09fbb6f9d296afaf2b
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-09-30 16:43:12 -0300

    Adicionada rota para página de produtos e pedidos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,9 @@ import { BrowserRouter as Router,
 import AppProvider from './Context/AppProvider';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Products from './Pages/Products';
+import Orders from './Pages/Orders';
+import NotFound from './Pages/NotFound';
 import './Styles/App.css';
 
 function App() {
@@ -14,9 +17,21 @@ function App() {
     <Router>
       <AppProvider>
         <Routes>
-          <Route exact path="/" element={ <Navigate to="/login" replace /> } />
+          <Route
+            exact
+            path="/"
+            element={ <Navigate to="/login" replace /> }
+          />
           <Route exact path="/login" element={ <Login /> } />
           <Route exact path="/register" element={ <Register /> } />
+          <Route
+            exact
+            path="/customer"
+            element={ <Navigate to="/customer/products" replace /> }
+          />
+          <Route exact path="/customer/products" element={ <Products /> } />
+          <Route exact path="/customer/orders" element={ <Orders /> } />
+          <Route exact path="/*" element={ <NotFound /> } />
         </Routes>
       </AppProvider>
     </Router>
