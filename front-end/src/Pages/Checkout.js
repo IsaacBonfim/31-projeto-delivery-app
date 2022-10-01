3f650376f61c2c160b82c2e593dd772b75060dcd
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-01 13:31:42 -0300

    Criada página provisória de Checkout

---
+++ front-end/src/Pages/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../Components/NavBar';
+
+function Checkout() {
+  return (
+    <div className="products-container">
+      <NavBar />
+      <h1>Tela de Checkout</h1>
+    </div>
+  );
+}
+
+export default Checkout;
