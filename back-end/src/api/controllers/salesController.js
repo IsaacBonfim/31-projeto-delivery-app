29effc98d914a794ebf377ce18329422735024ab
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-04 15:04:51 -0300

    Criada função no backend para retorno de pessoas vendedoras e adicionada validação do token na função de cadastro da venda

--- back-end/src/api/controllers/salesController.js
+++ back-end/src/api/controllers/salesController.js
@@ -2,16 +2,22 @@ const SalesService = require('../services/salesService');
 
 const SalesController = {
   async createSale(req, res, next) {
-    const { body } = req;
+    const { body, headers } = req;
 
     try {
-      const saleId = await SalesService.createSale(body);
+      const saleId = await SalesService.createSale(body, headers);
 
-      return res.status(200).json({ id: saleId });
+      return res.status(201).json({ id: saleId });
     } catch (error) {
       console.log(error);
       next(error);
     }
+  },
+
+  async sellerList(_req, res) {
+    const sellers = await SalesService.sellerList();
+
+    return res.status(200).json(sellers);
   },
 };
 
--- back-end/src/api/routes/salesRouter.js
+++ back-end/src/api/routes/salesRouter.js
@@ -5,6 +5,7 @@ const SalesController = require('../controllers/salesController');
 const SalesRouter = Router();
 
 SalesRouter.get('/products', ProductsController.getAllProducts);
+SalesRouter.get('/sellers', SalesController.sellerList);
 SalesRouter.post('/orders', SalesController.createSale);
 
 module.exports = SalesRouter;
