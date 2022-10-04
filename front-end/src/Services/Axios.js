3a3c61e10469b224d8c81cffca1909b7881c4951
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-04 15:17:30 -0300

    Criada função para requisição de pessoas vendedoras e uma função paraenviar o cadastro de um pedido

--- front-end/src/Services/Axios.js
+++ front-end/src/Services/Axios.js
@@ -13,3 +13,13 @@ export const requestProducts = async (endpoint) => {
   const { data } = await api.get(endpoint).catch((error) => error.response);
   return data;
 };
+
+export const requestSellers = async (endpoint) => {
+  const { data } = await api.get(endpoint).catch((error) => error.response);
+  return data;
+};
+
+export const requestOrder = (endpoint, body, token) => api
+  .post(endpoint, body, token)
+  .then(({ data }) => (data))
+  .catch((error) => error.response);
