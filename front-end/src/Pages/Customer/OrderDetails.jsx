680f36900a02ff9a9b034ca7d4b288847f576226
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-06 19:35:33 -0300

    Criada tela provisória de detalhes do pedido

---
+++ front-end/src/Pages/Customer/OrderDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../Components/NavBar';
+
+function OrderDetails() {
+  return (
+    <>
+      <NavBar />
+      <h1>Tela de Detalhes do Pedido</h1>
+    </>
+  );
+}
+
+export default OrderDetails;
