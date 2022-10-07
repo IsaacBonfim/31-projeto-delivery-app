945bddc692dd976f7bf634cf1c14efadf681cc9d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-07 12:21:35 -0300

    Implementada tela de pedidos

--- front-end/src/Pages/Customer/OrderDetails.jsx
+++ front-end/src/Pages/Customer/OrderDetails.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import appContext from '../../Context/AppContext';
 import NavBar from '../../Components/NavBar';
+import DetailsBoard from '../../Components/DetailsBoard';
 
 function OrderDetails() {
+  const { details, detailsRequest } = useContext(appContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    detailsRequest(id);
+  }, []);
+
   return (
-    <>
+    <div className="checkout-container">
       <NavBar />
-      <h1>Tela de Detalhes do Pedido</h1>
-    </>
+      { console.log(details) }
+
+      <h1>Detalhes do Pedido</h1>
+
+      { details.id !== undefined && (
+        <DetailsBoard details={ details } />
+      ) }
+
+      <span
+        data-testid="customer_order_details__element-order-total-price"
+      >
+        { `Total: R$ ${Number(details.totalPrice).toFixed(2).replace('.', ',')}` }
+      </span>
+    </div>
   );
 }
 
