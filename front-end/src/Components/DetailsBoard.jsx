47d76b6dfc5c8966a66fa0c18783571f45502ab7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-08 17:30:56 -0300

    Ajustado o DetailsBoard para utilizar um data-testid diferente de acordo com o tipo do usuário logado e adicionada função que dispara requisição de atualização do status do pedido

--- front-end/src/Components/DetailsBoard.jsx
+++ front-end/src/Components/DetailsBoard.jsx
@@ -1,17 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import appContext from '../Context/AppContext';
+import { getUser } from '../Services/LocalStorage';
+import { updateSaleRequest } from '../Services/Axios';
 
-const testId40 = 'customer_order_details__element-order-details-label-delivery-status';
-const testId41 = 'customer_order_details__element-order-table-item-number-';
-const testId43 = 'customer_order_details__element-order-table-quantity-';
-const testId44 = 'customer_order_details__element-order-table-unit-price-';
-const testId45 = 'customer_order_details__element-order-table-sub-total-';
+const testId38 = 'customer_order_details__element-order-details-label-seller-name';
 
 function DetailsBoard(props) {
   const { details } = props;
   const { products } = details;
+  const { role } = getUser();
 
   const [date, setDate] = useState('');
+  const { setDetails } = useContext(appContext);
+
+  async function update(status) {
+    const endpoint = `/orders/sale/${details.id}`;
+
+    const sale = await updateSaleRequest(endpoint, { status });
+
+    setDetails(sale);
+  }
 
   function getDate(saleDate) {
     const orderDate = saleDate.split('T')[0].split('-').reverse();
@@ -32,34 +41,64 @@ function DetailsBoard(props) {
       <thead>
         <tr>
           <th
-            data-testid="customer_order_details__element-order-details-label-order-id"
+            data-testid={ role === 'customer'
+              ? 'customer_order_details__element-order-details-label-order-id'
+              : 'seller_order_details__element-order-details-label-order-id' }
           >
             { `Pedido ${details.id}` }
           </th>
+          { role === 'customer' && (
+            <th
+              data-testid={ testId38 }
+            >
+              { `Vendedor: ${details.sellerName}` }
+            </th>
+          ) }
           <th
-            data-testid="customer_order_details__element-order-details-label-seller-name"
-          >
-            { `Vendedor: ${details.sellerName}` }
-          </th>
-          <th
-            data-testid="customer_order_details__element-order-details-label-order-date"
+            data-testid={ role === 'customer'
+              ? 'customer_order_details__element-order-details-label-order-date'
+              : 'seller_order_details__element-order-details-label-order-date' }
           >
             { date }
           </th>
           <th
-            data-testid={ testId40 }
+            data-testid={ role === 'customer'
+              ? 'customer_order_details__element-order-details-label-delivery-status'
+              : 'seller_order_details__element-order-details-label-delivery-status' }
           >
             { details.status }
           </th>
           <th>
-            <button
-              type="button"
-              disabled={ details.status !== 'Em Trânsito' }
-              data-testid="customer_order_details__button-delivery-check"
-              onClick={ () => console.log('Tô aqui!') }
-            >
-              MARCAR COMO ENTREGUE
-            </button>
+            { role === 'customer' && (
+              <button
+                type="button"
+                disabled={ details.status !== 'Em Trânsito' }
+                data-testid="customer_order_details__button-delivery-check"
+                onClick={ () => update('Entregue') }
+              >
+                MARCAR COMO ENTREGUE
+              </button>
+            ) }
+            { role === 'seller' && (
+              <>
+                <button
+                  type="button"
+                  disabled={ details.status !== 'Pendente' }
+                  data-testid="seller_order_details__button-preparing-check"
+                  onClick={ () => update('Preparando') }
+                >
+                  PREPARAR PEDIDO
+                </button>
+                <button
+                  type="button"
+                  disabled={ details.status !== 'Preparando' }
+                  data-testid="seller_order_details__button-dispatch-check"
+                  onClick={ () => update('Em Trânsito') }
+                >
+                  SAIU PARA ENTREGA
+                </button>
+              </>
+            ) }
           </th>
         </tr>
       </thead>
@@ -74,27 +113,37 @@ function DetailsBoard(props) {
         { products.map((product, index) => (
           <tr key={ index }>
             <th
-              data-testid={ `${testId41}${index}` }
+              data-testid={ role === 'customer'
+                ? `customer_order_details__element-order-table-item-number-${index}`
+                : `seller_order_details__element-order-table-item-number-${index}` }
             >
               { index + 1 }
             </th>
             <th
-              data-testid={ `customer_order_details__element-order-table-name-${index}` }
+              data-testid={ role === 'customer'
+                ? `customer_order_details__element-order-table-name-${index}`
+                : `seller_order_details__element-order-table-name-${index}` }
             >
               { product.name }
             </th>
             <th
-              data-testid={ `${testId43}${index}` }
+              data-testid={ role === 'customer'
+                ? `customer_order_details__element-order-table-quantity-${index}`
+                : `seller_order_details__element-order-table-quantity-${index}` }
             >
               { product.quantity }
             </th>
             <th
-              data-testid={ `${testId44}${index}` }
+              data-testid={ role === 'customer'
+                ? `customer_order_details__element-order-table-unit-price-${index}`
+                : `seller_order_details__element-order-table-unit-price-${index}` }
             >
               { `R$ ${product.price.replace('.', ',')}` }
             </th>
             <th
-              data-testid={ `${testId45}${index}` }
+              data-testid={ role === 'customer'
+                ? `customer_order_details__element-order-table-sub-total-${index}`
+                : `seller_order_details__element-order-table-sub-total-${index}` }
             >
               { `R$ ${product.subTotal.toFixed(2).replace('.', ',')}` }
             </th>
