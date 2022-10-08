329e5cc3a2d8ffaca282121b72974a2d6383acfa
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-08 17:29:12 -0300

    Ajustado o OrderCard para utilizar um data-testid diferente de acordo com o tipo do usuário logado

--- front-end/src/Components/OrderCard.jsx
+++ front-end/src/Components/OrderCard.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { getUser } from '../Services/LocalStorage';
 
 function OrderCard(props) {
   const { order } = props;
+  const { role } = getUser();
   const [date, setDate] = useState('');
 
   const navigate = useNavigate();
@@ -26,29 +28,44 @@ function OrderCard(props) {
     <div
       role="button"
       tabIndex={ order.id }
-      onClick={ () => navigate(`/customer/orders/${order.id}`) }
-      onKeyDown={ () => navigate(`/customer/orders/${order.id}`) }
+      onClick={ () => navigate(`/${role}/orders/${order.id}`) }
+      onKeyDown={ () => navigate(`/${role}/orders/${order.id}`) }
     >
       <span
-        data-testid={ `customer_orders__element-order-id-${order.id}` }
+        data-testid={ role === 'customer'
+          ? `customer_orders__element-order-id-${order.id}`
+          : `seller_orders__element-order-id-${order.id}` }
       >
         { order.id }
       </span>
       <span
-        data-testid={ `customer_orders__element-delivery-status-${order.id}` }
+        data-testid={ role === 'customer'
+          ? `customer_orders__element-delivery-status-${order.id}`
+          : `seller_orders__element-delivery-status-${order.id}` }
       >
         { order.status }
       </span>
       <span
-        data-testid={ `customer_orders__element-order-date-${order.id}` }
+        data-testid={ role === 'customer'
+          ? `customer_orders__element-order-date-${order.id}`
+          : `seller_orders__element-order-date-${order.id}` }
       >
         { date }
       </span>
       <span
-        data-testid={ `customer_orders__element-card-price-${order.id}` }
+        data-testid={ role === 'customer'
+          ? `customer_orders__element-card-price-${order.id}`
+          : `seller_orders__element-card-price-${order.id}` }
       >
         { `R$ ${Number(order.totalPrice).toFixed(2).replace('.', ',')}` }
       </span>
+      { role === 'seller' && (
+        <span
+          data-testid={ `seller_orders__element-card-address-${order.id}` }
+        >
+          { `${order.deliveryAddress}, Nº ${order.deliveryNumber}` }
+        </span>
+      ) }
     </div>
   );
 }
